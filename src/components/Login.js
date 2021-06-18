132ffebddf08cdfcd2497a501dd8531ebc4f8cac
Abhishek Dubey <ad99526@gmail.com>
2021-06-18 09:43:15 +0530

    added height to cha

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,51 +5,53 @@ import { Link } from "react-router-dom";
 import useAuthCheck from "../utils/useAuthCheck";
 
 const Login = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  useAuthCheck(props.auth.isAuthenticated, props.history);
-  const submitForm = (e) => {
-    e.preventDefault();
-    props.login({ email, password }, props.history);
-  };
-  if (props.auth.loading) {
-    return <h1>Loading...</h1>;
-  } else {
-    return (
-      <div className="signin">
-        <h1>Login</h1>
-        <form onSubmit={submitForm}>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit" onClick={submitForm}>
-            Login
-          </button>
-        </form>
-        <h6>
-          Don't have an account yet? <Link to="/signup">Signup</Link>
-        </h6>
-      </div>
-    );
-  }
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	useAuthCheck(props.auth.isAuthenticated, props.history);
+	const submitForm = (e) => {
+		e.preventDefault();
+		props.login({ email, password }, props.history);
+	};
+	if (props.auth.loading) {
+		return <h1>Loading...</h1>;
+	} else {
+		return (
+			<div className="signin">
+				<h1>Login</h1>
+				<form onSubmit={submitForm}>
+					<input
+						type="email"
+						name="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						placeholder="Registered Email"
+						required
+					/>
+					<input
+						type="password"
+						name="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="Registered Password"
+						required
+					/>
+					<button type="submit" onClick={submitForm}>
+						Login
+					</button>
+				</form>
+				<h6>
+					Don't have an account yet? <Link to="/signup">Signup</Link>
+				</h6>
+			</div>
+		);
+	}
 };
 const mapStateToProps = (state) => ({
-  auth: state.auth
+	auth: state.auth,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  login: (userData, history) => dispatch(loginUser(userData, history))
+	login: (userData, history) => dispatch(loginUser(userData, history)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,59 +5,61 @@ import { Link } from "react-router-dom";
 import useAuthCheck from "../utils/useAuthCheck";
 
 const Signup = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  useAuthCheck(props.auth.isAuthenticated, props.history);
-  const submitForm = (e) => {
-    e.preventDefault();
-    if (email && password) {
-      console.log("hi");
-      props.signUpUser({email, password}, props.history);
-    }
-  };
+	useAuthCheck(props.auth.isAuthenticated, props.history);
+	const submitForm = (e) => {
+		e.preventDefault();
+		if (email && password) {
+			console.log("hi");
+			props.signUpUser({ email, password }, props.history);
+		}
+	};
 
-  if (props.auth.loading) return <h1>Loading...</h1>;
-  else
-    return (
-      <div className="signup">
-        <h1>Signup</h1>
-        <form onSubmit={submitForm}>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+	if (props.auth.loading) return <h1>Loading...</h1>;
+	else
+		return (
+			<div className="signup">
+				<h1>Signup</h1>
+				<form onSubmit={submitForm}>
+					<input
+						type="email"
+						name="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+						placeholder="eg. johndoe@gmail.com"
+					/>
+					<input
+						type="password"
+						name="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="A strong password"
+						required
+					/>
 
-          <button type="submit" onClick={submitForm} className="">
-            Signup
-          </button>
-        </form>
-        <h6>
-          Already have an account?{" "}
-          <Link to="/login" style={{ textDecoration: "underline" }}>
-            Login
-          </Link>
-        </h6>
-      </div>
-    );
+					<button type="submit" onClick={submitForm} className="">
+						Signup
+					</button>
+				</form>
+				<h6>
+					Already have an account?{" "}
+					<Link to="/login" style={{ textDecoration: "underline" }}>
+						Login
+					</Link>
+				</h6>
+			</div>
+		);
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth
+	auth: state.auth,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  signUpUser: (userData, history) => dispatch(signUpUser(userData, history))
+	signUpUser: (userData, history) => dispatch(signUpUser(userData, history)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Signup);
